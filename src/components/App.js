93265d5746e078cb66c7478834323188850365a4
sanjeetsk <ssanjeetkumar67@gmail.com>
2023-07-09 00:33:20 +0530

    final

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ const Child = ({showModel, handleButton}) => {
       {
         showModel && <>
           <h3>Modal Content</h3>
-          <p>This is the modal content</p>
+          <p>This is the modal content.</p>
         </>
       } 
     </div>
